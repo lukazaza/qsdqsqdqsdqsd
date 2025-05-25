4666d6b1dd632dfa16d77e06439feabe2687cfd8
Reloadddddd <reload.contactme@gmail.com>
2025-05-25 17:53:42 +0200

    Update songs.ts

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -4,7 +4,7 @@ export const songs: Song[] = [
   {
     id: '1',
     title: 'Resonance - super ethereal slowed',
-    artist: 'Reload Ta Pub',
+    artist: 'InconuDu73',
     url: 'https://youtu.be/if8A0cHigp0?si=r5b2g2UEdNwKgqa0',
     coverArt: 'https://cdn.discordapp.com/attachments/1372686113826934855/1376226332815396894/Capture_decran_2025-05-25_175150.png?ex=68348e26&is=68333ca6&hm=4ce062c01248fcc4f39cd2ac72a25ebc54692162a6116382ac1493627f1e47dc',
   },
