8c36f646ca64b78c3ef8bd36e212fa4fe2bd7a54
Reloadddddd <reload.contactme@gmail.com>
2025-05-25 18:06:06 +0200

    Update songs.ts

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -1,19 +1,19 @@
 import { Song } from '../types';
 
 export const songs: Song[] = [
-  {
-    id: '1',
-    title: 'Resonance - super ethereal slowed',
-    artist: 'InconuDu73',
-    url: 'https://youtu.be/if8A0cHigp0?si=r5b2g2UEdNwKgqa0',
-    coverArt: 'https://cdn.discordapp.com/attachments/1372686113826934855/1376226332815396894/Capture_decran_2025-05-25_175150.png?ex=68348e26&is=68333ca6&hm=4ce062c01248fcc4f39cd2ac72a25ebc54692162a6116382ac1493627f1e47dc',
-  },
     {
     id: '2',
     title: 'GIMS - NINAO',
     artist: 'Gims',
     url: 'https://youtu.be/VT2qF97pQNw?si=ZCpCJiveVcyNhwPP',
     coverArt: 'https://cdn.discordapp.com/attachments/1376224460301930627/1376228272471408692/OIP.jpg?ex=68348ff5&is=68333e75&hm=dd61215a701c4e8719b1dc8e24b36898eff407b281d82bb9f064d309bde9013a&',
+  },
+     {
+    id: '1',
+    title: 'Resonance - super ethereal slowed',
+    artist: 'InconuDu73',
+    url: 'https://youtu.be/if8A0cHigp0?si=r5b2g2UEdNwKgqa0',
+    coverArt: 'https://cdn.discordapp.com/attachments/1372686113826934855/1376226332815396894/Capture_decran_2025-05-25_175150.png?ex=68348e26&is=68333ca6&hm=4ce062c01248fcc4f39cd2ac72a25ebc54692162a6116382ac1493627f1e47dc',
   },
       {
     id: '3',
