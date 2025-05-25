37d9e70c566157614aed33a9d56c0967287d8646
Reloadddddd <reload.contactme@gmail.com>
2025-05-25 17:52:23 +0200

    Update songs.ts

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -3,9 +3,9 @@ import { Song } from '../types';
 export const songs: Song[] = [
   {
     id: '1',
-    title: 'Reload - 1 Appel, 0 Problème !',
+    title: 'Resonance - super ethereal slowed',
     artist: 'Reload Ta Pub',
-    url: 'https://www.youtube.com/watch?v=0ycrBHxbolE',
-    coverArt: 'https://media.discordapp.net/attachments/1372686113826934855/1375909100017160402/ReloadFrance.png?ex=683366b4&is=68321534&hm=d7ada5e5d94e8ccd693d3eff30d21c4e0e9a7e71f8be12b85f1b47865727b594&=&format=webp&quality=lossless',
+    url: 'https://youtu.be/if8A0cHigp0?si=r5b2g2UEdNwKgqa0',
+    coverArt: 'https://cdn.discordapp.com/attachments/1372686113826934855/1376226332815396894/Capture_decran_2025-05-25_175150.png?ex=68348e26&is=68333ca6&hm=4ce062c01248fcc4f39cd2ac72a25ebc54692162a6116382ac1493627f1e47dc',
   },
 ];
