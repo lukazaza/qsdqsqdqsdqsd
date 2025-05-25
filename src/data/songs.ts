c705bd6eda41043a3caab16d81615608801b0030
Reloadddddd <reload.contactme@gmail.com>
2025-05-25 18:17:56 +0200

    Update songs.ts

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -29,13 +29,12 @@ export const songs: Song[] = [
     url: 'https://youtu.be/if8A0cHigp0?si=r5b2g2UEdNwKgqa0',
     coverArt: 'https://cdn.discordapp.com/attachments/1372686113826934855/1376226332815396894/Capture_decran_2025-05-25_175150.png?ex=68348e26&is=68333ca6&hm=4ce062c01248fcc4f39cd2ac72a25ebc54692162a6116382ac1493627f1e47dc',
   },
-        {
+    {
     id: '5',
     title: 'ça parle mal',
     artist: 'Bouss',
     url: 'https://youtu.be/UWbh7n6akIU?si=pg3VrHCDgp85yMPk',
     coverArt: 'https://cdn.discordapp.com/attachments/1376224460301930627/1376231003055456406/3e65128ed256678164e92c0e3de7d888.png?ex=68349280&is=68334100&hm=8742e5b7164d5ac2a364fb640d7d7e5fdf4471940e2894ecc3cd22025fbc2a13&',
-  },
   },
     {
     id: '6',
